88927a09e991c3149ad83980cedb6ab706795d94
vlabsdev-iitd <vlabsdev.iitd@gmail.com>
2021-03-01 11:13:46 +0530

    exp2

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -89,22 +89,50 @@
 
 
     const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1. which type of loading is applied in flexure strength test?", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "Single point loading", ///// Write the option 1 inside double quotes
+                b: "Four-point loading", ///// Write the option 2 inside double quotes
+                c: "Three-point loading", ///// Write the option 1 inside double quotes
+                d: "Uniform distributed load", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "b" ///// Write the correct option inside double quotes
+        },
+
+        {
+            question: "2. A beam having cross section 100mmx100mm and span length 400 mm, load at failure is equal to 5KN in flexural strength test failure type A. Flexural strength will", ///// Write the question inside double quotes
+            answers: {
+                a: "2 MPa", ///// Write the option 1 inside double quotes
+                b: "4 MPa", ///// Write the option 2 inside double quotes
+                c: "6 MPa", ///// Write the option 1 inside double quotes
+                d: "8 MPa", ///// Write the option 2 inside double quotes
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
 
-    {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
-      answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
-    },
+        {
+            question: "3. For two beam specimens of same size, load at failure is reported same but with different fracture type. Higher flexural strength will be of specimen having", ///// Write the question inside double quotes
+            answers: {
+                a: "Type A failure", ///// Write the option 1 inside double quotes
+                b: "Type B failure", ///// Write the option 2 inside double quotes
+                c: "Equal flexural strength for both", ///// Write the option 1 inside double quotes
+                d: "Cannot say", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+
+         
+
+         
+
+    // {
+    //   question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+    //   answers: {
+    //     a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
+    //     b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
+    //     c: "<img src='images/1c.PNG'>",      },
+    //   correctAnswer: "c"                ///// Write the correct option inside double quotes
+    // },
 
 
         
